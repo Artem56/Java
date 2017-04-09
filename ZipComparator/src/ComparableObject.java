5065024f1cbbbbd22a069d6cd23ceaf1a318af73
Artem Solomatin <artemsolomatin1905@gmail.com>
2017-04-10 00:54:29 +0300

    Удалены лишние архивы и папки

--- ZipComparator/src/ComparableObject.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Artem Solomatin on 10.04.17.
- * ZipComparator
- */
-public interface ComparableObject <T> {
-    FileStatus compareToObject(T object);
-
-    T getComparedObject();
-}
--- ZipComparator/src/FileStatus.java
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Artem Solomatin on 10.04.17.
- * ZipComparator
- */
-public enum FileStatus {
-    CHANGED, RENAMED , EQUALS , NOT_EQUALS;
-}
