795b9c5bff1ff51e342bdefb39c69204da3e2142
Artem Solomatin <artemsolomatin1905@gmail.com>
2017-04-17 13:20:50 +0300

    Добавлена работа с массивами. Оптимизирована работа с коллекциями. Нужно протестировать

--- DataStructuresSpeed/src/Main.java
+++ DataStructuresSpeed/src/Main.java
@@ -23,10 +23,9 @@ public class Main {
     private static int NUMBER;
 
     int[] intArray;
-    Integer[] integerArray;
 
     ArrayList<Integer> arrayList;
-    LinkedList<Integer> linkedList;
+    LinkedList linkedList;
     List<Integer> synchronizedList;
 
     TreeSet<Integer> treeSet;
@@ -37,56 +36,54 @@ public class Main {
 
     ArrayList<Collection<Integer>> usedCollections;
 
-    LinkedHashMap<String, Integer> insertResults;
-    LinkedHashMap<String, Integer> removeResults;
-    LinkedHashMap<String, Integer> searchResults;
+    LinkedHashMap<String, Long> insertResults;
+    LinkedHashMap<String, Long> removeResults;
+    LinkedHashMap<String, Long> searchResults;
 
     long totalTime;
 
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws ClassNotFoundException {
         long startTime = System.currentTimeMillis();
 
         NUMBER = Integer.parseInt(args[0]);
 
         Main test = new Main();
 
-        //test.arrayInitialization();
+        test.arrayInitialization();
         test.initialization();
         for(Collection<Integer> current: test.usedCollections){
-            test.insertSpeed(current);
-            test.removeSpeed(current);
-            test.reduceCollection(current);
-            test.searchSpeed(current);
+            test.insertSpeed(current, null);
+            test.removeSpeed(current, null);
+            test.reduceCollection(current, null);
+            test.searchSpeed(current, null);
         }
-        test.totalTime = System.currentTimeMillis() - startTime;
 
         test.print();
 
-
+        test.totalTime = System.currentTimeMillis() - startTime;
 
     }
 
     private Main(){
         //массивы
         intArray = new int[NUMBER * 11];
-        integerArray = new Integer[NUMBER * 11];
 
         //списки
-        arrayList = new ArrayList<>();
+        arrayList = new ArrayList<>(NUMBER * 11);
         linkedList = new LinkedList<>();
-        synchronizedList = Collections.synchronizedList(new ArrayList<Integer>());
+        synchronizedList = Collections.synchronizedList(new ArrayList<Integer>(NUMBER * 11));
 
         //деревья
         treeSet = new TreeSet<>();
         synchronizedTree = Collections.synchronizedSet(new TreeSet<Integer>());
 
         //хэш-таблицы
-        hashSet = new HashSet<>();
-        synchronizedHash = Collections.synchronizedSet(new HashSet<Integer>());
+        hashSet = new HashSet<>(NUMBER * 11);
+        synchronizedHash = Collections.synchronizedSet(new HashSet<Integer>(NUMBER * 11));
 
         //список использованных коллекций
-        usedCollections = new ArrayList<>();
+        usedCollections = new ArrayList<>(10);
 
         usedCollections.add(arrayList);
         usedCollections.add(linkedList);
@@ -106,16 +103,14 @@ public class Main {
         int i;
         for(i = 0;i < NUMBER * 10;i++){            //заполнение массивов начальными NUMBER * 10 элементами
             Integer tmp = (int)(Math.random() * NUMBER * 5);
-
             intArray[i] = tmp;
-            integerArray[i] = tmp;
         }
     }
 
     private void initialization(){
         int i;
         for(i = 0;i < NUMBER * 10;i++){     //заполнение коллекций начальными NUMBER * 10 элементами
-            Integer tmp = (int)(Math.random() * NUMBER);
+            Integer tmp = (int)(Math.random() * NUMBER * 5);
 
             arrayList.add(tmp);
             linkedList.add(tmp);
@@ -129,59 +124,124 @@ public class Main {
         }
     }
 
-    private void insertSpeed(Collection<Integer> collection){
-        int i;
-        long startTime = System.currentTimeMillis();
-        for(i = 0;i < NUMBER;i++){
-            Integer tmp = (int)(Math.random() * NUMBER);
-            collection.add(tmp);
+
+
+    private long insertSpeed(Collection<Integer> collection, int[] array){
+        long resultTime = 0;
+        long resultArrayTime = 0;
+        long quantumTime = 0;
+        for(int i = 0;i < NUMBER;i++){
+            Integer tmp = (int)(Math.random() * NUMBER * 5);
+            if(collection != null) {
+                quantumTime = System.currentTimeMillis();
+                collection.add(tmp);
+                resultTime += (System.currentTimeMillis() - quantumTime);
+            }else if(array != null){
+                quantumTime = System.currentTimeMillis();
+                array[i] = tmp;
+                resultArrayTime += (System.currentTimeMillis() - quantumTime);
+            }
         }
-        long endTime = System.currentTimeMillis();
-        insertResults.put(collection.getClass().toString(), (int)(endTime - startTime));
+        if(collection != null){
+            insertResults.put(collection.getClass().toString(), resultTime);
+        }
+        return array != null ? resultArrayTime : -1;
     }
 
-    private void searchSpeed(Collection<Integer> collection){
-        int i;
-        long startTime = System.currentTimeMillis();
-        for(i = 0;i < NUMBER;i++){
+    private long searchSpeed(Collection<Integer> collection, int[] array){
+        long resultTime = 0;
+        long resultArrayTime = 0;
+        long quantumTime = 0;
+        for(int i = 0;i < NUMBER;i++){
             Integer tmp = (int)(Math.random() * NUMBER);
-            collection.contains(tmp);
+            if(collection != null) {
+                quantumTime = System.currentTimeMillis();
+                collection.contains(tmp);
+                resultTime += (System.currentTimeMillis() - quantumTime);
+            }else if(array != null){
+                quantumTime = System.currentTimeMillis();
+                for (int anArray : array) {
+                    if (anArray == tmp) break;
+                }
+                resultArrayTime += (System.currentTimeMillis() - quantumTime);
+            }
         }
-        long endTime = System.currentTimeMillis();
-        searchResults.put(collection.getClass().toString(), (int)(endTime - startTime));
+        if(collection != null){
+            searchResults.put(collection.getClass().toString(), resultTime);
+        }
+        return array != null ? resultArrayTime : -1;
     }
 
-    private void removeSpeed(Collection<Integer> collection){
-        int i;
-        long startTime = System.currentTimeMillis();
-        for(i = 0;i < NUMBER;i++){
+    private long removeSpeed(Collection<Integer> collection, int[] array){
+        long resultTime = 0;
+        long resultArrayTime = 0;
+        long quantumTime = 0;
+        for(int i = 0;i < NUMBER;i++){
             Integer tmp = (int)(Math.random() * NUMBER);
-            collection.remove(tmp);
+            if(collection != null) {
+                quantumTime = System.currentTimeMillis();
+                collection.remove(tmp);
+                resultTime += (System.currentTimeMillis() - quantumTime);
+            }else if(array != null){
+                quantumTime = System.currentTimeMillis();
+                for (int anArray : array) {
+                    if (anArray == tmp){
+                        anArray = 0;
+                        break;
+                    }
+                }
+                array[i] = tmp;                                                   //   ЭТО НЕ УДАЛЕНИЕ
+                resultArrayTime += (System.currentTimeMillis() - quantumTime);
+            }
         }
-        long endTime = System.currentTimeMillis();
-        removeResults.put(collection.getClass().toString(), (int)(endTime - startTime));
+        if(collection != null){
+            removeResults.put(collection.getClass().toString(), resultTime);
+        }
+
+        return array != null ? resultArrayTime : -1;
     }
 
-    private void reduceCollection(Collection<Integer> collection){
-        int i;
-        for(i = 0;i < NUMBER * 9;i++){       //очистим структуру, оставив NUMBER элементов
-            Integer tmp = (int)(Math.random() * NUMBER);
-            collection.remove(tmp);
+    private void reduceCollection(Collection<Integer> collection, int[] array){//очистим структуру, оставив NUMBER элементов
+        int i = 0;
+        if(collection != null) {
+            Iterator<Integer> itr = collection.iterator();
+            while (itr.hasNext() && i < NUMBER * 9) {
+                itr.next();
+                itr.remove();
+                i++;
+            }
+            System.out.println("test " + collection.size());
+        }
+
+
+
+
+        //Collection newCollection = collection.getClass().newInstance();
+        if(array != null) {
+            int[] newArray = new int[NUMBER];
+            System.arraycopy(array, 0, newArray, 0, NUMBER);
+            intArray = newArray;
         }
     }
 
-    private void print(){
+    private void print() throws ClassNotFoundException {
         System.out.println("Total time for test is: " + totalTime / 1000 + " seconds");
         System.out.println("Insert speed");
-        for(Map.Entry<String, Integer> entry : insertResults.entrySet()){
+        System.out.println("class int[] : " + insertSpeed(null, intArray));
+        for(Map.Entry<String, Long> entry : insertResults.entrySet()){
             System.out.println(entry.getKey() + " " + entry.getValue());
         }
-        System.out.println("Search speed");
-        for(Map.Entry<String, Integer> entry : searchResults.entrySet()){
-            System.out.println(entry.getKey() + " " + entry.getValue());
-        }
+
         System.out.println("Remove speed");
-        for(Map.Entry<String, Integer> entry : removeResults.entrySet()){
+        System.out.println("class int[] : " + removeSpeed(null, intArray));
+        for(Map.Entry<String, Long> entry : removeResults.entrySet()){
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+        reduceCollection(null, intArray);
+
+        System.out.println("Search speed");
+        System.out.println("class int[] : " + searchSpeed(null, intArray));
+        for(Map.Entry<String, Long> entry : searchResults.entrySet()){
             System.out.println(entry.getKey() + " " + entry.getValue());
         }
     }
